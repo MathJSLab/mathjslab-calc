b5ed4eb7a6c049d32c4ebc37bd8523201aa394ba
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-07-30 19:24:39 -0300

    Dependecies updated (`mathjslab` 2.3.0). Update in version 0.0.4 on Thu, 30 Jul 2026 22:24:34 GMT

--- CHANGES.md
+++ CHANGES.md
@@ -3,6 +3,10 @@
 All notable changes to this project will be documented in this file. This
 project adheres to [Semantic Versioning](http://semver.org/).
 
+## 0.0.4
+
+- Dependecies updated (`mathjslab` 2.3.0).
+
 ## 0.0.3
 
 - DOI and other badges added.
--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Tue, 28 Jul 2026 03:38:38 GMT. */
+/* Web components. File generated at Thu, 30 Jul 2026 22:24:07 GMT. */
 export * from './calc-prompt/calc-prompt.component';
 export * from './calc-prompt-list/calc-prompt-list.component';
 export * from './calculator-keypad/calculator-keypad.component';
